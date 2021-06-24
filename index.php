9c342e41fb9f41d193217476c044f387c277b220
TaitXtended <marveloustchimusoro@gmail.com>
2021-06-24 04:03:26 +0100

    my site with no php

--- index.php
+++ index.php
@@ -1,38 +1,260 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Fly in</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+	<meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<ul class="fly hidden">
-	<li>T</li>
-	<li>a</li>
-	<li>i</li>
-	<li>t</li>
-	<li style="width: 20px"> </li>
-	<li>X</li>
-	<li>t</li>
-	<li>e</li>
-	<li>n</li>
-	<li>d</li>
-	<li>e</li>
-	<li>d</li>
+	<!-- Stylesheets
+	============================================= -->
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&amp;display=swap" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="css/style(2).css" type="text/css" />
+	<link rel="stylesheet" href="css/swiper.css" type="text/css" />
+	<link rel="stylesheet" href="css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+	<link rel="stylesheet" href="all.min.css" type="text/css" />
+	<link rel="stylesheet" href="css/custom.css" type="text/css" />
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
 	
-</ul>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-<div>
-    <a href="dice.php">DICE</a>
+	<title>Tait Online</title>
+	<style>
+		#contact{
+  			background-color: #1abc9c;
+			  text-align: center;
+				padding:5%;
+				margin-bottom:-100px;
+			}
+			#contact input{
+				color:#000;
+				text-align: center;
+				width: 60%;
+				height:50px;
+				background:transparent;
+				margin-bottom: 1%;
+			}
+			#contact textarea{
+				min-height:300px;
+				max-height:500px;
+				overflow: auto;
+				width: 60%;
+			}
+			button{
+				width: 60%;
+				background:#333333;
+			}
+			.social-media{
+				margin-top:10%;
+				width: 70%;
+				margin-right: 125%;
+					
+			}
+			.social-media a{
+
+
+ height: 50px;
+ width: 50px;
+ padding:20px 5px;
+ border-radius:50%;
+
+}
+
+
+.social-media a:hover{
+ background-color: #1abc9c;
+ color:#FFFFFF;
+ box-shadow:2px 2px 12px rgba(0,0,0,0.2);
+ transition:all ease 1.7s;
+}
+
+ 
+	</style>
+</head>
+<body class="stretched has-plugin-isotope has-plugin-parallax" >
+	<div id="wrapper" class="clearfix">
+
+<header id="header" class="full-header transparent-header sticky-header dark" data*sticky-class="not-dark">
+	<div id="header-wrap" class="not-dark">
+		<div class="container">
+			<div class="header-row top-search-parent">
+				<div class="" style="height: 100px; width: 100px; font-size: 70px;">
+				<i class="fab fa-tumblr-square"></i>
+				</div>
+				<div  style=" width: 60%; margin-left:2%; font-size: 50px; font-weight: bold; text-align: center;">
+					TAIT XTENDED
+					
+				</div>
+				<!-- menu links -->
+				
+					<div style="float:right; " >
+						<ul style="list-style:none; ">
+							<li>
+								<a  href="student/index.php" >
+									Portfolio
+								</a>
+							</li>
+							
+							
+							<li>
+								<a  href="#contact" >
+									Contact Us
+								</a>
+							</li>
+
+						</ul>
+					</div>	
+			</div>
+		</div>
+	</div>
+</header>
+
+
+	<section id="content" style=" background-image:  linear-gradient( rgba(0,0,0,.7),rgba(0,0,0,.5)), url(img/code.jpg); background-size: cover; margin-top: 04% ; padding-bottom:4%; overflow:hidden;">
+		<div class="content-wrap">
+			<div class="container">
+				
+					<div class="col-lg-8">
+						<div class="">
+
+						
+					</div>
+					
+				</div>
+				
+			</div>
+			
+		</div>
+
+<div  >
+			
+
+<div class=" dark " style="width: 100%;">
+<div class="container">
+
+
+<div class="slider-caption mobilee " style="  transform: translate3d(10px, -40px,0);  color: #000;">
+	
+	<h2 class="fadeInUp animated" data-animate="fadeInUp" data-delay="200" style="margin-left: 70%;"> 
+	LET'S GO DIGITAL	</h2>
+	
 </div>
-<script type="text/javascript" src="jquery.min.js"></script>
-<script type="text/javascript">
+</div>
+</div>
+</div>
+</div>
 
-	$(function(){
-		setTimeout(function(){
-			$('.fly').removeClass('hidden');
-		}, 1100);
-	})();
-</script>
+
+		<div id="about" class="section my-0">
+					<div class="row mt-4 col-mb-50 ">
+						<div style="margin-bottom: 30px; padding: 20px 50px; margin-top: -90px; background-color: #333333; color: #fff; width: 100%;">
+							<center><h1 style="color: #fff; margin-bottom: 0px;">About Tait</h1></center>
+							<div style="margin-left:10%;">
+							Tait Xtended is a Software developer who specialises in the development of custom made and Dynamic Websites.
+							</div>
+						</div>
+					</div>
+					<div style="margin-bottom: 30px; padding: 20px 50px; background-color: #ccc; color: #333333; width: 100%;">
+							<center><h1 style=" margin-bottom: 0px;">Services</h1></center>
+							
+					
+						<div class="row mt-4 col-mb-50">
+						
+								
+
+							<div class="col-lg-4">
+								<i class="fal fa-globe fa-5x" style="margin-bottom: 15px;"></i>
+								<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+							
+									<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+									
+									<h4>Website Design and Development</h4>
+								</div>
+								<p>The design and development of web based applications and projects that are dynamic and customised to suit your needs</p>
+							</div>
+
+						</div>
+							<div class="col-lg-4">
+								<i class="fas fa-tablet-android-alt fa-5x" style="margin-bottom: 15px;"></i>
+								<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+								
+									<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+									
+									<h4>Mobile App Development</h4>
+								</div>
+								 
+								<ul style="padding-left: 8%;">
+									<li class="menu-item">
+										Creation of applications that run on all <i class="fab fa-android fa-2x"></i> Android and <i class="fab fa-apple fa-2x"></i> iOS devices	</li>
+								</ul>
+							</div>
+
+						</div>
+						<div class="col-lg-4">
+								<i class="fas fa-chalkboard-teacher fa-5x" style="margin-bottom: 15px;"></i>
+								<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+								<i class=""></i>
+									<div class="heading-block border-bottom-0" style="margin-bottom: 15px;">
+									
+									<h4>Basic Programming Lessons</h4>
+								</div>
+								 
+								<ul style="padding-left: 8%;">
+									<li class="menu-item">
+										To provide and facilitate  efficient, affordable high quality programming skills through exposure to several real world applications</li>
+								</ul>
+							</div>
+
+						</div>
+				
+					
+				</div>
+
+		
+	</section>
+	<div id="contact">
+		
+		
+
+			<h2 style="text-align: center; color: #333333;">Send Us Feedback</h2>
+			<section >
+				<form method="post" action="#">
+					
+						<input name="name" placeholder=" Name " type="text" required="" />
+				
+					
+						<input name="email" placeholder="Email" type="email" required />
+			
+						<textarea name="message" placeholder="Message" required=""></textarea>
+					<div  >
+					 <button class="btn  btn-dark ">Send <i class="fa fa-paper-plane"></i></button>
+					</div>
+						
+					</form>
+			</section>
+	      
+  </div>
+
+	</div>
+
+
+	<div class="social-media">
+	<a href="https://www.facebook.com/Eleazar-Designs-196472517762831"><i class="fab fa-facebook fa-3x"></i></a>
+	<a href="#"><i class="fab fa-twitter fa-3x"></i></a>
+	<a href="#"><i class="fab fa-instagram fa-3x"></i></a>
+	<a href="#"><i class="fab fa-youtube fa-3x"></i></a>
+	</div>
+
+	</div>
+	<footer>
+		<div class="footer">
+			<p><p>Copyright © 2021. All rights reserved. Vehicle Inspectorate Department</p></p>
+		</div>
+	</footer>
 </body>
 </html>
